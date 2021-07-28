c9a4a3c16f926685bf5dbaae5927bd9e93aca510
Edwin Lima <edwin.limaa@gmail.com>
2021-07-27 21:54:11 -0300

    Conceito de estado local do componente calculator.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { sum } from '../store/Calculator/Calculator.actions';
 
 function Calculator (){
+    const dispatch = useDispatch();
     const result = useSelector(state => state.calculator)
+    const [a, setA] = useState(0);
+    const [b, setB] = useState(0);
+    
     return <>
         <div>
-            <input type="text" placeholder="a"/>
-            <input type="text" placeholder="b"/>
-            <button>somar</button>
+            <input 
+                type="text" placeholder="a"
+                value={a} 
+                onChange={(event) => setA(Number(event.target.value)) }
+            />
+            <input type="text" placeholder="b" 
+                value={b} 
+                onChange={(event) => setB(Number(event.target.value)) }
+            />
+            <button
+                onClick={()=>{
+                    dispatch(sum(a,b))                    
+                }}
+            >somar</button>
             <button>subtrair</button>              
         </div>
         <div>
